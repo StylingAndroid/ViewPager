af3e5185c57fd2bc9837f69d54791e640b6170b3
Mark Allison <mark@stylingandroid.com>
2011-09-02 08:56:22 +0100

    Completed Part 2

--- src/com/stylingandroid/viewpager/ViewPagerAdapter.java
+++ src/com/stylingandroid/viewpager/ViewPagerAdapter.java
@@ -1,10 +1,17 @@
 package com.stylingandroid.viewpager;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import android.content.Context;
 import android.os.Parcelable;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import com.jakewharton.android.viewpagerindicator.TitleProvider;
@@ -39,18 +46,30 @@ public class ViewPagerAdapter extends PagerAdapter implements TitleProvider
     @Override
     public Object instantiateItem( View pager, int position )
     {
-        TextView v = new TextView( context );
-        v.setText( titles[ position ] );
-        ((ViewPager)pager).addView( v, 0 );
+        ListView v = new ListView( context );
+        String[] from = new String[] { "str" };
+        int[] to = new int[] { android.R.id.text1 };
+        List<Map<String, String>> items =
+            new ArrayList<Map<String, String>>();
+        for ( int i = 0; i < 20; i++ )
+        {
+            Map<String, String> map =
+                new HashMap<String, String>();
+            map.put( "str", String.format( "Item %d", i + 1 ) );
+            items.add( map );
+        }
+        SimpleAdapter adapter = new SimpleAdapter( context, items,
+            android.R.layout.simple_list_item_1, from, to );
+        v.setAdapter( adapter );
+        ( (ViewPager) pager ).addView( v, 0 );
         return v;
     }
- 
+     
     @Override
     public void destroyItem( View pager, int position, Object view )
     {
-        ((ViewPager)pager).removeView( (TextView)view );
-    }
- 
+        ( (ViewPager) pager ).removeView( (ListView) view );
+    } 
     @Override
     public boolean isViewFromObject( View view, Object object )
     {
