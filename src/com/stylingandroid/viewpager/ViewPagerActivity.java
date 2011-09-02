8d3e43b31f2651ec623a84f512b50bc085ea34a2
Mark Allison <mark@stylingandroid.com>
2011-09-02 07:35:55 +0100

    Initial commit

---
+++ src/com/stylingandroid/viewpager/ViewPagerActivity.java
@@ -0,0 +1,13 @@
+package com.stylingandroid.viewpager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ViewPagerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
